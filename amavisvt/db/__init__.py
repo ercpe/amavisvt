af040b367a246d1c149a24fc5e80a7dbc8dddc0c
Johann Schmitz <johann@j-schmitz.net>
2016-08-05 05:51:11 +0200

    Converted tabs to spaces

--- amavisvt/db/__init__.py
+++ amavisvt/db/__init__.py
@@ -4,9 +4,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 try:  # pragma: no cover
-	from amavisvt.db.sqlitedb import AmavisVTDatabase
-	Database = AmavisVTDatabase
+    from amavisvt.db.sqlitedb import AmavisVTDatabase
+    Database = AmavisVTDatabase
 except ImportError:  # pragma: no cover
-	logger.info("Database is NOT supported (could not import module sqlite3)")
-	from amavisvt.db.base import NoopDatabase
-	Database = NoopDatabase
+    logger.info("Database is NOT supported (could not import module sqlite3)")
+    from amavisvt.db.base import NoopDatabase
+    Database = NoopDatabase
